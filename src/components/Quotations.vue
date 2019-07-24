a1cd700bdba1b25fd1b88dc06d643fcb4f4bfcba
Patrick Mendonça Oliveira <oliveira.patrick94@gmail.com>
2019-07-23 21:34:32 -0300

    Component Quotations

---
+++ src/components/Quotations.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="quotations">
+        <h1>{{ title }}</h1>
+        <HelloWorld />
+    </div>
+</template>
+
+<script>
+import HelloWorld from './HelloWorld'
+
+export default {
+  name: 'Quotations',
+  data () {
+    return {
+      title: 'Cotações'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Quotations from '@/components/Quotations'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Quotations',
+      component: Quotations
     }
   ]
 })

d7b94e2d37272fe8ee6365f5559ac501391051ea
Patrick Mendonça Oliveira <oliveira.patrick94@gmail.com>
2019-07-25 00:04:39 -0300

    login template

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Quotations from '@/components/Quotations'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Quotations',
       component: Quotations
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })

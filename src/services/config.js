adfaad4da48e55d64a8532bac5a5332e125eac75
Patrick Mendonça Oliveira <oliveira.patrick94@gmail.com>
2019-08-09 23:03:11 -0300

    add CORS

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'https://api.hgbrasil.com/finance/quotations?key=04b8901a'
+  baseURL: 'https://api.hgbrasil.com/finance/quotations?format=json-cors&key=04b8901a'
 })
